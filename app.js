1c1ca621ce38cc3c66b073ab785a91c63f2ac3a8
YanYahui <1219125967@qq.com>
2020-12-22 17:44:14 +0800

    vant-weapp

--- app.js
+++ app.js
@@ -1,47 +1,62 @@
 //app.js
-const api = require('./utils/api');
-import req_ok  from './utils/common'
+import {
+  getopenId
+} from './utils/api'
+import {
+  req_ok
+} from './utils/common'
+import {
+  EncryptContent
+} from './utils/aes'
+
+
 App({
-  onLaunch: function () {
+  onLaunch: async function () {
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-        if (res.code) {
-          api.getopenId({wxCode:res.code}).then(res=>{
-            if(res.code === req_ok){
-              console.info("登录成功返回的openId：" + res.data.openId);
-            }else{
-              //没有获取到登录信息
-              // wx.navigateTo({
-              //   url: '../login/index'
-              // })
-            }
-            
-          })
-        }else{
+    const re = await wx.login({
+      timeout: 3000,
+    })
+    if (re.code) {
+      const a = await getopenId({
+        wxCode: re.code
+      })
+      if (a.code === req_ok) {
+        console.log('获取成功:', a)
+        const id = a.data.userId;
+        this.globalData.openid = a.data.openId
+        this.globalData.userId = id
+        this.globalData.wxInfo = a.data.infojson
+        
+        if(id === null || id === ''){
           wx.navigateTo({
-            url: '..pages/login/index'
+            url: '/login/index'
           })
         }
-
-
+        const str = EncryptContent(JSON.stringify(a.data))
+        this.globalData.userInfo = str
+      } else {
+        console.log('获取失败:', a)
+        wx.navigateTo({
+          url: '/login/index'
+        })
       }
-    })
+      console.log('获取openid返回:', a);
+    }
+
     // 获取用户信息
     wx.getSetting({
       success: res => {
+        console.log('用户微信信息:', res)
         if (res.authSetting['scope.userInfo']) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
             success: res => {
+              console.log('用户微信信息2:', res)
               // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+              this.globalData.wxInfo = res.userInfo
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
               if (this.userInfoReadyCallback) {
@@ -54,7 +69,9 @@ App({
     })
   },
   globalData: {
-    userInfo: null,
-    costType:[]//类型
+    wxInfo: null,
+    userId:null,
+    userInfo:null,
+    costType: [] //类型
   }
 })
